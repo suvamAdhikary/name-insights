8b71bae4ddeba5414fe5ded123fb2f8273e93e06
Suvam Adhikary <adhikary.saheb408@gmail.com>
2024-03-23 02:24:08 +0530

    commented dummy replay from API call

--- app/api/fetchNameInsights.ts
+++ app/api/fetchNameInsights.ts
@@ -8,17 +8,17 @@ interface NameInsights {
 }
 
 const fetchNameInsights = async (name: string): Promise<NameInsights> => {
-  return {
-    age: 31,
-    gender: "male",
-    country: [
-      { country_id: "BD", probability: 0.39273515280123705 },
-      { country_id: "NP", probability: 0.3004132906144831 },
-      { country_id: "IN", probability: 0.1966362457283266 },
-      { country_id: "AE", probability: 0.013382520721044263 },
-      { country_id: "QA", probability: 0.009690751310144616 },
-    ],
-  };
+  // return {
+  //   age: 31,
+  //   gender: "male",
+  //   country: [
+  //     { country_id: "BD", probability: 0.39273515280123705 },
+  //     { country_id: "NP", probability: 0.3004132906144831 },
+  //     { country_id: "IN", probability: 0.1966362457283266 },
+  //     { country_id: "AE", probability: 0.013382520721044263 },
+  //     { country_id: "QA", probability: 0.009690751310144616 },
+  //   ],
+  // };
   try {
     // Make API calls to fetch age, gender, and country
     const ageResponse = await axios.get(`https://api.agify.io?name=${name}`);
